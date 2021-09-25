fd17a045888c46529ce23464995738522914cf6b
LiChiFungTom <72788072+LiChiFungTom@users.noreply.github.com>
2021-09-25 20:51:58 +0800

    Create test2.java

---
+++ test2.java
@@ -0,0 +1,7 @@
+public class test{
+    public static void main(String[] args) {
+    
+        CircleClassAdapter adapter = new CircleClassAdapter(10,15,25.5); 
+        PrintCircle.printCircle(adapter);
+    }
+}
