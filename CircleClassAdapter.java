2d79f913025f8c245359e27b4ba3a53beb3c6985
LiChiFungTom <72788072+LiChiFungTom@users.noreply.github.com>
2021-09-25 20:50:45 +0800

    Update CircleClassAdapter.java

--- CircleClassAdapter.java
+++ CircleClassAdapter.java
@@ -3,24 +3,27 @@ import java.awt.Point;
 
 public class CircleClassAdapter extends OldCircleImpl implements NewCircle{
 
-    
+    int x;
+    int y;
 
-    public CircleClassAdapter(double a,double b,double c)){
-        super();
+    public CircleClassAdapter(double a,double b,double c){
+        super(a,b,c);
     }
 
     public double getRadius( ){
         double radius;
         double[] coeff = this.getCoeff();
-
         radius = Math.sqrt( coeff[2] );
         return radius;
     }
     
 	public Point getCenter( ){
-
+        double [] coeff = new double[3];
+        coeff = this.getCoeff();
+        x = (int)coeff[0];
+        y = (int)coeff[1];
+        Point p = new Point(x,y);
+        return p;
 
     }
 }
-
-
